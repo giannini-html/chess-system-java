0b5e4d44459ca10ccbac50135b122f624c235111
Gustavo Giannini <gustavo-giannini@hotmail.com>
2021-10-07 11:10:24 -0300

    Implementing possible moves of Rock

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,7 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		
+
 		while (true) {
 			try {
 				UI.clearScreen();
@@ -22,20 +22,20 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
-				ChessPiece capturedPiece =chessMatch.performChessMove(source, target);
-			} catch(ChessException e) {
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			} catch(InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
-				
+
 		}
 	}
 }
